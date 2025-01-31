3cd1516688476d97072579348313297827f1488d
Abdulkadir Salad <abdulkadir.s.salad1@gmail.com>
2025-01-31 10:17:31 +0000

    added middleware to app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const cors = require('cors')
+const logger = require('./logger')
 
 const app = express();
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+app.use(logger)
 
 module.exports = app;
